ba28ad45b39e3ddfb696cc2765797cd3f03703c1
tsubodayo529 <925tsubokawayoichi@gmail.com>
2021-11-18 14:49:31 +0900

    ユーザーデータの登録と参照を追加

--- Assets/PlayfabLogin2.cs
+++ Assets/PlayfabLogin2.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
+using UnityEngine.UI;
 
 public class PlayfabLogin2 : MonoBehaviour
 {
 
+//スコアボード表示確認のため
     public int score;
+
+//UserNameを送信する処理の確認のためのInputField
+    public InputField inputName;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -69,6 +76,39 @@ public class PlayfabLogin2 : MonoBehaviour
         foreach (var item in result.Leaderboard){
             Debug.Log("Rank : " +  item.Position + "ID : " + item.PlayFabId + "Score : " + item.StatValue);
         }
+    }
+
+
+//ユーザーデータの登録
+    public void SaveUserData() {
+        string userName = inputName.text;
+        var request = new UpdateUserDataRequest{
+            Data = new Dictionary<string, string>{
+                {"Name", userName},
+                {"Age", "21"},
+                {"Favorites", "Game"}
+            }
+        };
+        PlayFabClientAPI.UpdateUserData(request, OnDataSend, OnError);
+    }
+
+
+///ユーザーデータの参照
+    public void GetUserData(){
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest(), OnDataRecieved, OnError);
+    }
+
+    void OnDataRecieved(GetUserDataResult result){
+        Debug.Log("Recieved user data!");
+        if(result.Data != null && result.Data.ContainsKey("Name") &&result.Data.ContainsKey("Age") &&result.Data.ContainsKey("Favorites")){
+            Debug.Log(result.Data["Name"].Value + "は" + result.Data["Age"].Value + "歳で" + result.Data["Favorites"].Value + "が趣味です");
+        } else{
+            Debug.Log("Player data not complete!");
+        }
+    }
+
+    void OnDataSend(UpdateUserDataResult result){
+        Debug.Log("Successful user data send!");
     }
 
 
